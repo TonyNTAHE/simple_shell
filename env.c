c9f3db992e2fc1113091840f1a048fe50b331309
TonyNTAHE <tonyntahe@gmail.com>
2023-08-23 20:38:43 +0200

    Completing all the checks

---
+++ env.c
@@ -0,0 +1,31 @@
+#include "shell.h"
+/**
+ * env - print the enivornment.
+ *
+ * Return: nothing.
+ */
+void env(void)
+{
+	while (*environ)
+	{
+		printf("%s\n", *environ);
+		environ++;
+	}
+}
+
+/**
+ * cd - change directory.
+ * @args: the argument.
+ * Return: 1 on success or 0 on failure.
+ */
+int cd(char **args)
+{
+	int signal = chdir(args[1]);
+	
+	if (signal == -1)
+	{
+		perror("./hsh");
+		return (0);
+	}
+	return (1);
+}
